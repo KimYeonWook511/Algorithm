e7b68df92c5c5f68e9fef93a9200d2c7dd80a35a
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-01-04 23:36:03 +0900

    [Bronze V] Title: A+B - 7, Time: 260 ms, Memory: 18680 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		int[] a = new int[testcase];
+		int[] b = new int[testcase];
+		
+		for (int i = 0; i < testcase; i++) 
+		{
+			a[i] = sc.nextInt();
+			b[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < testcase; i++)
+		{
+			System.out.println("Case #" + (i + 1) + ": " + (a[i] + b[i]));			
+		}
+		
+		sc.close();
+	}
+}
