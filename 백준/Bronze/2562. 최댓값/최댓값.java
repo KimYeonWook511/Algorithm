ab1fb9c629f5bbb31ff1469d42daf9de3c3ce9d4
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-01-08 19:38:36 +0900

    [Bronze III] Title: 최댓값, Time: 140 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int max = 0;
+		int idx = 0;
+		
+		for (int i = 0; i < 9; i++) {
+			int v = Integer.parseInt(br.readLine());
+			
+			if (v > max) { 
+				max = v;
+				idx = i + 1;
+			}
+		}
+		
+		bw.write(Integer.toString(max) + "\n");
+		bw.write(Integer.toString(idx));
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
