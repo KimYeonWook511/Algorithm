89e2fe3b08277d4702c17940c78e51445627eaf9
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-01-17 01:18:12 +0900

    [Bronze III] Title: ACM 호텔, Time: 164 ms, Memory: 16500 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int h = Integer.parseInt(st.nextToken());
+			String w = st.nextToken();
+			int n = Integer.parseInt(st.nextToken());
+			
+			String y, x;
+			
+			if (n % h == 0) {
+				y = Integer.toString(h);
+				x = Integer.toString(n / h);
+				
+			} else {
+				y = Integer.toString(n % h);
+				x = Integer.toString(n / h + 1);
+				
+			}
+
+			if (x.length() == 1) {
+				x = "0" + x;
+			}
+			
+			bw.write(y + x + "\n");
+		}
+		
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
