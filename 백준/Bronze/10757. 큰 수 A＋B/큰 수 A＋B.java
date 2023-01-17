879d9b178ca7c4943cef6ca5f8d7663ab1255841
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-01-18 00:32:55 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 248 ms, Memory: 17432 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		bw.write(new BigInteger(st.nextToken()).abs().add(new BigInteger(st.nextToken())).abs().toString());
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
