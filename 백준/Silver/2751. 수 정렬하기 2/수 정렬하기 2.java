f5cc5e3062e7f21f5e75942629dd9dc9e722e161
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-01-19 19:49:37 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1664 ms, Memory: 171556 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		for (int run = 0; run < n; run++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		list.sort(Comparator.naturalOrder());
+		
+		for (int i = 0; i < list.size(); i++) {
+			bw.write(list.get(i) + "\n");
+		}
+		
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
