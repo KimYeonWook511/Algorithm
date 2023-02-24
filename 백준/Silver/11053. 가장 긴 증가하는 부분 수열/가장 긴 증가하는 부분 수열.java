c812b73ef7aa71693b55a349c2920e80ce1b3b65
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-02-24 17:40:30 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 144 ms, Memory: 14732 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int arr[] = new int[n];
+		int dp[] = new int[n];
+		int max = 1;
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = 1;
+		}
+		
+		for (int i = 1; i < n; i++) {
+			for (int k = 0; k < i; k++) {
+				if (arr[k] < arr[i] && dp[i] < dp[k] + 1) {
+					dp[i] = dp[k] + 1;
+					
+					max = Math.max(max, dp[i]);
+				}
+			}
+		}
+		
+		bw.write(Integer.toString(max));
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
