4d428321d0c83ff5da8e5b2d86139c5236b9f1fa
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-03-05 21:46:30 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+
+		int n = Integer.parseInt(br.readLine());
+		int dp[] = new int[n + 2];
+		dp[1] = 1;
+		dp[2] = 3;
+		
+		for (int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+		}
+		
+		bw.write(Integer.toString(dp[n]));
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
