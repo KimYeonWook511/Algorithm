57a6dbd3a7030acd885ca1aa6eb3773e09bb8868
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-03-29 15:31:12 +0900

    [Gold V] Title: 연속합 2, Time: 280 ms, Memory: 24944 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -22,7 +22,7 @@ public class Main {
 		for (int i = 1; i < n; i++) {
 			int num = Integer.parseInt(st.nextToken());
 			
-			dp[i][0] = dp[i - 1][0] + num > num ? dp[i - 1][0] + num : num;
+			dp[i][0] = Math.max(num, dp[i - 1][0] + num);
 			dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + num);
 			
 			max = Math.max(max, Math.max(dp[i][0], dp[i][1]));
