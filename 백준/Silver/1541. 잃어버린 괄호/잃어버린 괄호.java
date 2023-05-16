a211de2823657d2b117dda678ce28ffe7c0bd975
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-05-16 16:59:31 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws IOException {
+		
+		String str[] = br.readLine().split("-");
+		int operand[] = new int[25];
+		int result = 0;
+		
+		for (int i = 0; i < str.length; i++) {
+			int num = 0;
+			
+			for (int idx = 0; idx < str[i].length(); idx++) {
+				char c = str[i].charAt(idx);
+				
+				if (c == '+') {
+					operand[i] += num;
+					num = 0;
+					
+				} else {
+					num = num * 10 + c - '0';
+				}
+			}
+			
+			operand[i] += num;
+		}
+		
+		result = operand[0];
+		
+		for (int i = 1; i < 25; i++) {
+			result -= operand[i];
+		}
+		
+		System.out.println(result);
+		
+		br.close();
+	}
+}
