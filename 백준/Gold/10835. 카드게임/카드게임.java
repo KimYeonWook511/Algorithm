fb68648f30886d69a424bece0bbc280a087ca430
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-05-25 21:24:44 +0900

    [Gold V] Title: 카드게임, Time: 256 ms, Memory: 33280 KB -BaekjoonHub

--- 백준/Gold/10835. 카드게임/카드게임.java
+++ 백준/Gold/10835. 카드게임/카드게임.java
@@ -1,52 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	static int n;
-	static int a[];
-	static int b[];
-	static int dp[][];
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int n, a[], b[], dp[][];
 	
 	public static void main(String[] args) throws IOException {
 		
-		n=Integer.parseInt(br.readLine());
-		a=new int[n];
-		b=new int[n];
-		dp=new int[n][n];
-		StringTokenizer st=new StringTokenizer(br.readLine());
+		n = Integer.parseInt(br.readLine());
+		a = new int[n];
+		b = new int[n];
+		dp = new int[n][n];
 		
-		for (int i=0; i<n; i++) {
-			a[i]=Integer.parseInt(st.nextToken());
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		
+		for (int i = 0 ; i < n; i++) {
+			a[i] = Integer.parseInt(st1.nextToken());
+			b[i] = Integer.parseInt(st2.nextToken());
+			
+			for (int c = 0; c < n; c++) {
+				dp[i][c] = -1; // 아직 계산되지 않았다는 뜻
+			}
 		}
 		
-		st=new StringTokenizer(br.readLine());
-		for (int i=0; i<n; i++) {
-			b[i]=Integer.parseInt(st.nextToken());
-		}
+		System.out.println(cal(0, 0));
 		
-		for (int i=0; i<n; i++)
-			Arrays.fill(dp[i],-1);
-		
-		System.out.println(v(0,0));
 		br.close();
 	}
 	
-	public static int v(int left, int right) {
-		if (left==n || right==n) {
-			return 0;
-		}
+	public static int cal(int left, int right) {
+		if (left == n || right == n) return 0;
 		
-		if (dp[left][right]!=-1) {
-			return dp[left][right];
-		}
+		if (dp[left][right] != -1) return dp[left][right]; // 이미 계산 되었음
 		
-		if (a[left]>b[right]) dp[left][right]=v(left,right+1) + b[right];
-		else dp[left][right]=Math.max(v(left+1,right), v(left+1,right+1));
-		
+		if (a[left] > b[right]) dp[left][right] = cal(left, right + 1) + b[right];
+		else dp[left][right] = Math.max(cal(left + 1, right), cal(left + 1, right + 1)); 
+			
 		return dp[left][right];
 	}
 }
