cd397f766cb5d1aa4ec3a05278e9032a698edebc
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-06-07 22:52:15 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 160 ms, Memory: 18152 KB -BaekjoonHub

---
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final int MOD = 1_000_000_007;
+	
+	public static void main(String[] args) throws IOException {
+
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int dp[][] = new int[Math.max(n, m) + 2][Math.max(n, m) + 2];
+		
+		dp[0][0] = 1;
+		
+		for (int r = 1; r <= n; r++) {
+			for (int c = 1; c <= m; c++) {
+				dp[r][c] = (dp[r - 1][c] + dp[r][c - 1]) % MOD;
+				dp[r][c] = (dp[r][c] + dp[r - 1][c - 1]) % MOD;
+			}
+		}
+		
+		System.out.println(dp[n][m]);
+		
+		br.close();
+	}
+}
