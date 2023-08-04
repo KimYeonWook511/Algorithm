1f5d3289c67651e063cb932c5d303beb840e320f
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-08-04 16:29:36 +0900

    [Silver III] Title: 안테나, Time: 620 ms, Memory: 42624 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws IOException {
+		
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken()); 
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[(n - 1) / 2]);
+		
+		br.close();
+	}
+}
