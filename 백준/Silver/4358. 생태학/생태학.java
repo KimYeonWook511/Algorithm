5e55d75aa8df60db3afb045a479bbd7673fad93d
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-09-18 10:10:52 +0900

    [Silver II] Title: 생태학, Time: 728 ms, Memory: 84644 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Map<String, Integer> map = new HashMap<>();
+        float total = 0;
+        
+        while (true) {
+            String str = br.readLine();
+
+            if (str == null || str.length() == 0) break;
+            
+            map.put(str, map.getOrDefault(str, 0) + 1);
+            total++;
+        }
+
+        List<String> list = new ArrayList<>(map.keySet());
+
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            String key = list.get(i);
+
+            sb.append(key).append(" ").append(String.format("%.4f", map.get(key) / total * 100)).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+        br.close();
+    }
+}
