d086de2a20f770b9104c21eaa50b556a4bccd77d
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-10-29 00:27:01 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 284 ms, Memory: 24116 KB -BaekjoonHub

--- 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
@@ -7,18 +7,19 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int arr[] = new int[n + 1];
-        int sum[] = new int[n + 1];
+        int sum = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            sum[i] = sum[i - 1] + arr[i];
+            sum += arr[i];
         }
 
         long result = 0;
 
         for (int i = 1; i <= n; i++) {
-            result += (sum[n] - sum[i]) * arr[i];
+            sum -= arr[i];
+            result += sum * arr[i];
         }        
 
         System.out.println(result);
