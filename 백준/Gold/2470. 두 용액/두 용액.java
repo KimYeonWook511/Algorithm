37d252a813a998aa390b6701109034886b982842
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-11-11 23:27:14 +0900

    [Gold V] Title: 두 용액, Time: 356 ms, Memory: 27976 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int min = 0;
+        int max = arr.length - 1;
+
+        int valMin = arr[0];
+        int valMax = arr[max];
+        int val = Math.abs(arr[0] + arr[max]);
+
+        while (min < max) {
+            int v = arr[min] + arr[max];
+            int absV = Math.abs(v);
+
+            if (absV < val) {
+                val = absV;
+                valMin = arr[min];
+                valMax = arr[max];
+            }
+
+            if (v < 0) min++;
+            else if (v > 0) max--;
+            else break;
+        }
+
+        sb.append(valMin).append(" ").append(valMax);
+        System.out.println(sb.toString());
+
+        br.close();
+    }
+}
