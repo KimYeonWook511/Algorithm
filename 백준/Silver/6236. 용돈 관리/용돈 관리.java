ac14a02cf7aaeada834e4c9b8716f359ae40be88
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-11-18 16:30:48 +0900

    [Silver II] Title: 용돈 관리, Time: 268 ms, Memory: 22668 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int left = 0;
+        int right = 1_000_000_000;
+        int arr[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine()); 
+
+            arr[i] = num;
+            left = Math.max(left, num);
+        }
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            int cnt = 1;
+            int sum = mid;
+
+            for (int i = 0; i < N; i++) {
+                sum -= arr[i];
+
+                if (sum < 0) {
+                    sum = mid - arr[i];
+                    cnt++;
+                }
+            }
+
+            if (cnt <= M) right = mid;
+            else left = mid + 1;
+        }
+
+        System.out.println(right);
+
+        br.close();
+    }
+}
