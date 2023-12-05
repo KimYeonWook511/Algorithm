3e4dd0e6540b17595ba7fe8410d7a269f34f02b8
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-12-05 13:29:51 +0900

    [Silver II] Title: 정보대 등산, Time: 520 ms, Memory: 44120 KB -BaekjoonHub

---
+++ 백준/Silver/12841. 정보대 등산/정보대 등산.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int cross[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            cross[i] = Integer.parseInt(st.nextToken());
+        }
+    
+        st = new StringTokenizer(br.readLine());
+
+        long left[] = new long[n];
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        long right[] = new long[n];
+        for (int i = 1; i < n; i++) {
+            right[i] = right[i - 1] + Long.parseLong(st.nextToken());
+        }
+
+        int point = 0;
+        long min = 10_000_000_000l;
+
+        for (int i = 0; i < n; i++) {
+            long d = right[n - 1] - right[i] + left[i] + cross[i];
+            
+            if (d < min) {
+                min = d;
+                point = i + 1;
+            }
+        }
+
+        System.out.println(point + " " + min);
+
+        br.close();
+    }
+}
