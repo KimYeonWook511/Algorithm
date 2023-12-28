a848de2300695dfc0ab8a44b131bf5859e2b1671
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2023-12-28 22:12:25 +0900

    [Silver III] Title: 에너지 드링크, Time: 336 ms, Memory: 25568 KB -BaekjoonHub

---
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double result = 0;
+        int max = 0;
+
+        for (int run = 0; run < N; run++) {
+            int num = Integer.parseInt(st.nextToken());
+            max = Math.max(max, num);
+
+            result += num / 2.0;
+        }
+
+        System.out.println(result + max / 2.0);
+
+        br.close();
+    }
+}
