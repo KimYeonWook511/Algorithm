963af8ec2f0a4e45890d699ff9f52860f806113f
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-01-08 23:32:34 +0900

    [Silver III] Title: N과 M (5), Time: 332 ms, Memory: 29036 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, arr[], result[];
+    static boolean chk[];
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        chk = new boolean[N];
+        result = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        func(0);
+
+        System.out.println(sb.toString());
+
+        br.close();
+    }
+
+    static void func(int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (chk[i]) continue;
+
+            chk[i] = true;
+            result[cnt] = arr[i];
+            func(cnt + 1);
+            chk[i] = false;
+        }
+    }
+}
