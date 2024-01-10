62a80480af08d7a7cb8d75e8ded1847db1137a77
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-01-10 23:08:12 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -4,14 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
 		
-		System.out.println((a + b) % c);
-		System.out.println(((a % c) + (b % c)) % c);
-		System.out.println((a * b) % c);
-		System.out.println(((a % c) * (b % c)) % c);
+		System.out.println((A + B) % C);
+		System.out.println(((A % C) + (B % C)) % C);
+		System.out.println((A * B) % C);
+		System.out.println(((A % C) * (B % C)) % C);
+		
 		sc.close();
 	}
 }
