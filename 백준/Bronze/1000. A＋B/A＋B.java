96d2d7c322469b9d5fc0e599aa9e52e4591723bf
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-01-10 23:08:22 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -4,10 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		int A = sc.nextInt();
+		int B = sc.nextInt();
 		
-		System.out.println(a + b);
+		System.out.println(A + B);
+		
 		sc.close();
 	}
 }
