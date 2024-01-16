5fed3447bd947f3494d2612cce7cf4948ddfcc7e
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-01-16 19:50:25 +0900

    [Silver III] Title: 여우는 어떻게 울지?, Time: 128 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.java
+++ 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.java
@@ -1,12 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
