6a5a06ed9a7d510412113aea2bdc68c24b766b54
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-01-29 22:40:48 +0900

    [Silver I] Title: 오목, Time: 152 ms, Memory: 16192 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -7,9 +7,6 @@ public class Main {
 	static int row, col, temp;
 
 	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("Test5.txt"));
-		//여기에 코드를 작성하세요.
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
@@ -26,13 +23,9 @@ public class Main {
 		flag : for (int r = 1; r <= N; r++) {
 			for (int c = 1; c <= N; c++) {
 				if (arr[r][c] != 0) {
-//					if (up(r, c)) break flag;
 					if (down(r, c)) break flag;
-//					if (left(r, c)) break flag;
 					if (right(r, c)) break flag;
 					
-//					if (crossUpLeft(r, c)) break flag;
-//					if (crossUpRight(r, c)) break flag;
 					if (crossDownLeft(r, c)) break flag;
 					if (crossDownRight(r, c)) break flag;
 				}
@@ -45,28 +38,6 @@ public class Main {
 		
 		br.close();
 	}
-	
-//	static boolean up(int r, int c) {
-//		// 6돌 체크
-//		if (r + 1 <= N && arr[r + 1][c] == arr[r][c]) return false;
-//		if (r - 5 > 0 && arr[r - 5][c] == arr[r][c]) return false;
-//		
-//		int cnt = 1;
-//		
-//		for (int i = 1; i < 5; i++) {
-//			if (r - i < 1) break;
-//			if (arr[r - i][c] == arr[r][c]) cnt++; 
-//		}
-//		
-//		if (cnt == 5) {
-//			row = r - 4;
-//			col = c;
-//			temp = arr[r][c];
-//			return true;
-//		}
-//		
-//		return false;
-//	}
 	
 	static boolean down(int r, int c) {
 		// 6돌 체크
@@ -90,28 +61,6 @@ public class Main {
 		return false;
 	}
 	
-//	static boolean left(int r, int c) {
-//		// 6돌 체크
-//		if (c + 1 <= N && arr[r][c + 1] == arr[r][c]) return false;
-//		if (c - 5 > 0 && arr[r][c - 5] == arr[r][c]) return false;
-//		
-//		int cnt = 1;
-//		
-//		for (int i = 1; i < 5; i++) {
-//			if (c - i < 1) break;
-//			if (arr[r][c - i] == arr[r][c]) cnt++; 
-//		}
-//		
-//		if (cnt == 5) {
-//			row = r;
-//			col = c - 4;
-//			temp = arr[r][c];
-//			return true;
-//		}
-//		
-//		return false;
-//	}
-	
 	static boolean right(int r, int c) {
 		// 6돌 체크
 		if (c - 1 > 0 && arr[r][c - 1] == arr[r][c]) return false;
@@ -133,50 +82,6 @@ public class Main {
 		
 		return false;
 	}
-	
-//	static boolean crossUpLeft(int r, int c) {
-//		// 6돌 체크
-//		if (r + 1 <= N && c + 1 <= N && arr[r + 1][c + 1] == arr[r][c]) return false;
-//		if (r - 5 > 0 && c - 5 > 0 && arr[r - 5][c - 5] == arr[r][c]) return false;
-//		
-//		int cnt = 1;
-//		
-//		for (int i = 1; i < 5; i++) {
-//			if (r - i < 1 || c - i < 1) break;
-//			if (arr[r - i][c - i] == arr[r][c]) cnt++; 
-//		}
-//		
-//		if (cnt == 5) {
-//			row = r - 4;
-//			col = c - 4;
-//			temp = arr[r][c];
-//			return true;
-//		}
-//		
-//		return false;
-//	}
-	
-//	static boolean crossUpRight(int r, int c) {
-//		// 6돌 체크
-//		if (r + 1 <= N && c - 1 > 0 && arr[r + 1][c - 1] == arr[r][c]) return false;
-//		if (r - 5 > 0 && c + 5 <= N && arr[r - 5][c + 5] == arr[r][c]) return false;
-//		
-//		int cnt = 1;
-//		
-//		for (int i = 1; i < 5; i++) {
-//			if (r - i < 1 || c + i > N) break;
-//			if (arr[r - i][c + i] == arr[r][c]) cnt++; 
-//		}
-//		
-//		if (cnt == 5) {
-//			row = r;
-//			col = c;
-//			temp = arr[r][c];
-//			return true;
-//		}
-//		
-//		return false;
-//	}
 	
 	static boolean crossDownLeft(int r, int c) {
 		// 6돌 체크
