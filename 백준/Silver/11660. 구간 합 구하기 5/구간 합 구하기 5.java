6ea853c2b4b36ca1a1abac5f31dd3cd48180be2e
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-02-01 00:55:48 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 836 ms, Memory: 117976 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,45 +1,44 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	static int N, M, arr[][];
+	static int x1, y1, x2, y2;
 
 	public static void main(String[] args) throws IOException {
-      
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int n = Integer.parseInt(st.nextToken());
-      int m = Integer.parseInt(st.nextToken());
-      int arr[][] = new int[n + 1][n + 1];
-      
-      for (int r = 1; r <= n; r++) {
-    	  st = new StringTokenizer(br.readLine());
-    	  int sum = 0; // 같은 행의 합
-    	  
-    	  for (int c = 1; c <= n; c++) {
-    		  sum += Integer.parseInt(st.nextToken());
-    		  arr[r][c] = sum + arr[r - 1][c]; // 같은 행의 합 + 같은 열의 합 -> 직사각형의 합 
-    	  }
-      }
-      
-      for (int run = 0; run < m; run++) {
-    	  st = new StringTokenizer(br.readLine());
-    	  int x1 = Integer.parseInt(st.nextToken());
-    	  int y1 = Integer.parseInt(st.nextToken());
-    	  int x2 = Integer.parseInt(st.nextToken());
-    	  int y2 = Integer.parseInt(st.nextToken());
-    	  
-    	  // x2, y2까지의 직사각형 - (x1 - 1, y2까지의 직사각형 + x2, y1 - 1까지의 직사각형) + x1 - 1, y1 - 1까지의 직사각형(빼는 직사각형끼리 겹치는 직사각형)
-    	  bw.write(arr[x2][y2] - (arr[x1 - 1][y2] + arr[x2][y1 - 1]) + arr[x1 - 1][y1 - 1] + "\n");
-      }
-      
-      bw.flush();
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-      br.close();
-      bw.close();
-   }
+		arr = new int[N + 1][N + 1];
+
+		for (int r = 1; r <= N; r++) {
+			st = new StringTokenizer(br.readLine());
+
+			for (int c = 1; c <= N; c++) {
+				arr[r][c] = arr[r - 1][c] + arr[r][c - 1] - arr[r - 1][c - 1] + Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int run = 0; run < M; run++) {
+			st = new StringTokenizer(br.readLine());
+			x1 = Integer.parseInt(st.nextToken());
+			y1 = Integer.parseInt(st.nextToken());
+			x2 = Integer.parseInt(st.nextToken());
+			y2 = Integer.parseInt(st.nextToken());
+
+			sb.append(arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1]).append("\n");
+		}
+
+		System.out.println(sb);
+
+		br.close();
+	}
 }
