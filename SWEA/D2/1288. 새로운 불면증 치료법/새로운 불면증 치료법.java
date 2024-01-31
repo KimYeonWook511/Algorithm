38947ce9bd1e795a0a61ac87d91de129110a3358
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-01-31 20:24:53 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 102 ms, Memory: 18,216 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,39 +1,38 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int T = Integer.parseInt(br.readLine());
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+
+	static int T, N, num, bit;
+	
+	public static void main(String[] args) throws Exception {
+		T = Integer.parseInt(br.readLine());
 
         for (int t = 1; t <= T; t++) {
-            int N = Integer.parseInt(br.readLine());
-            int tempN = N;
-
-            boolean chk[] = new boolean[10];
-            int cnt = 10;
+            N = Integer.parseInt(br.readLine());
             
-            while (cnt > 0) {
-                String str = Integer.toString(tempN);
-                int len = str.length();
-                
-                for (int i = 0; i < len; i++) {
-                    int idx = str.charAt(i) - '0';
+            bit = (1 << 10) - 1;
+            num = N;
 
-                    if (!chk[idx]) {
-                        chk[idx] = true;
-                        cnt--;
-                    }
-                }
-
-                tempN += N;
+            while (true) {
+            	String str = Integer.toString(num);
+            	int len = str.length();
+            	
+            	for (int i = 0; i < len; i++) {
+            		bit &= ~(1 << str.charAt(i) - '0');
+            	}
+            	
+            	if (bit == 0) break;
+            	
+            	num += N;
             }
-
-            sb.append("#").append(t).append(" ").append(tempN - N).append("\n");
+            
+            sb.append("#").append(t).append(" ").append(num).append("\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
 
         br.close();
     }
