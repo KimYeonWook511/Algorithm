abc086865992f207c27dbe0c5ce08d456356facf
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-02-02 21:19:16 +0900

    [Silver IV] Title: 카드2, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -5,16 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 
 		int n = Integer.parseInt(br.readLine());
 		int p = 0;
 		
-		for (int i = 0; Math.pow(2, i) < n; i++) {
+		for (int i = 0; (1 << i) < n; i++) {
 			p++;
 		}
-		
-		System.out.println(2 * n - (int)Math.pow(2, p));
+
+		System.out.println((n << 1) - (1 << p));
 		
 		br.close();
 	}
