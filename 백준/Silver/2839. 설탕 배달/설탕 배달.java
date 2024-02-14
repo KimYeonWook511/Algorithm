f8734b39bd87b76cb549a20d4ed35f216e2e5b59
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-02-14 19:48:32 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,33 +1,25 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	static int N, end, cnt = -1;
+	
+	public static void main(String[] args) throws Exception {
+		N = Integer.parseInt(br.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
-		int copyN = n;
-		int max = n / 3;
-		int cnt = -1;
+		end = N / 3;
 		
-		for (int i = 0; i <= max; i++) {
-			n = copyN;
-			n = n - 3 * i;
+		for (int i = 0; i <= end; i++) {
+			if ((N - 3 * i) % 5 != 0) continue;
 			
-			if (n % 5 == 0) {
-				cnt = i + n / 5;
-				break;
-			}
+			cnt = i + (N - 3 * i) / 5;
+			break;
 		}
 		
-		bw.write(Integer.toString(cnt));
-		bw.flush();
-
+		System.out.println(cnt);
+		
 		br.close();
-		bw.close();
 	}
 }
