5e52cae0b03e5b6f1edc5170119f4c292aee3678
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-02-15 23:02:19 +0900

    [Gold V] Title: 강의실 배정, Time: 636 ms, Memory: 64900 KB -BaekjoonHub

--- 백준/Gold V/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold V/11000. 강의실 배정/강의실 배정.java
@@ -6,19 +6,22 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+        @Override
+        public int compare(int[] o1, int[] o2) {
+            return Integer.compare(o1[1], o2[1]);
+        }
+    });
+
+    static int N, arr[][];
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o1[1], o2[1]);
-            }
-        });
-
-        int N = Integer.parseInt(br.readLine());
-        int arr[][] = new int[N][2];
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
