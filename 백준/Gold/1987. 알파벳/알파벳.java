0ec2a11869bf6474b9f362892cd9aa9b9b7db416
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-02-20 20:51:18 +0900

    [Gold IV] Title: 알파벳, Time: 152 ms, Memory: 15096 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -6,8 +6,7 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 
-	static int R, C, arr[][], max;
-	static boolean chk[] = new boolean[26];
+	static int R, C, arr[][], max, d[][] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}, nr, nc, chk[][];
 	static String str;
 
 	public static void main(String[] args) throws Exception {
@@ -17,46 +16,41 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 
+		chk = new int[R][C];
+		
 		arr = new int[R][C];
 		for (int r = 0; r < R; r++) {
 			str = br.readLine();
-			
+
 			for (int c = 0; c < C; c++) {
 				arr[r][c] = str.charAt(c) - 'A';
 			}
 		}
 		
-		func(0, 0, 0);
+		func(0, 0, 1, (1 << arr[0][0]));
 
 		System.out.println(max);
 
 		br.close();
 	}
 	
-	static void func(int r, int c, int cnt) {
-		if (r < 0 || c < 0) {
-			max = Math.max(max, cnt);
+	static void func(int r, int c, int cnt, int bit) {
+		if (max == 26) return;
+		
+		max = Math.max(max, cnt);
 
-			return;
+		chk[r][c] = bit;
+		
+		for (int i = 0; i < 4; i++) {
+			nr = r + d[i][0];
+			nc = c + d[i][1];
+			
+			if (nr < 0 || nc < 0) continue;
+			if (nr >= R || nc >= C) continue;
+			if ((bit & (1 << arr[nr][nc])) != 0) continue;
+			if ((bit | (1 << arr[nr][nc])) == chk[nr][nc]) continue;
+			
+			func(nr, nc, cnt + 1, bit | (1 << arr[nr][nc]));
 		}
-
-		if (r >= R || c >= C) {
-			max = Math.max(max, cnt);
-
-			return;
-		}
-
-		if (chk[arr[r][c]]) {
-			max = Math.max(max, cnt);
-
-			return;
-		}
-
-		chk[arr[r][c]] = true;
-		func(r - 1, c, cnt + 1);
-		func(r, c + 1, cnt + 1);
-		func(r + 1, c, cnt + 1);
-		func(r, c - 1, cnt + 1);
-		chk[arr[r][c]] = false;
 	}
 }
