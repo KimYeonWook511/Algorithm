bf9c1650595d27e13ef076aea25c55bd6a87e017
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-02-27 20:39:38 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,16 +1,10 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine());
 		int result[] = new int[3];
@@ -26,16 +20,14 @@ public class Main {
 			rgb[0] += Math.min(result[1], result[2]);
 			rgb[1] += Math.min(result[0], result[2]);
 			rgb[2] += Math.min(result[0], result[1]);
+			
 			result[0] = rgb[0];
 			result[1] = rgb[1];
 			result[2] = rgb[2];
 		}
-		
-		bw.write(Integer.toString(Math.min(result[0], Math.min(result[1], result[2]))));
-		bw.flush();
+
+		System.out.println(Math.min(result[0], Math.min(result[1], result[2])));
 
 		br.close();
-		bw.close();
 	}
-	
 }
