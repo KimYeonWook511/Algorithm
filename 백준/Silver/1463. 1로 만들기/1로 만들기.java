cfa0da69431febc1ae89826619c950928b1af89e
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-02-27 20:39:31 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18136 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,44 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
-		int arr[] = new int[n + 3];
-		arr[1] = 0;
-		arr[2] = 1;
-		arr[3] = 1;
+		int N = Integer.parseInt(br.readLine());
 		
-		for (int i = 4; i < n + 3; i++) {
+		int dp[] = new int[N + 3];
+		dp[2] = 1;
+		dp[3] = 1;
+		
+		for (int i = 4; i < N + 3; i++) {
 			if (i % 2 == 0) {
-				if (i % 3 == 0) {
-					// 2와 3 둘 다 나누어 떨어질 경우
-					arr[i] = Math.min(Math.min(arr[i - 1], arr[i / 2]), Math.min(arr[i - 1], arr[i / 3])) + 1;
-					
-				} else {
-					arr[i] = Math.min(arr[i - 1], arr[i / 2]) + 1;
-				}
+				if (i % 3 == 0) dp[i] = Math.min(Math.min(dp[i - 1], dp[i >> 1]), Math.min(dp[i - 1], dp[i / 3])) + 1;
+				else dp[i] = Math.min(dp[i - 1], dp[i >> 1]) + 1;
 				
 			} else if (i % 3 == 0) {
-				arr[i] = Math.min(arr[i - 1], arr[i / 3]) + 1;
+				dp[i] = Math.min(dp[i - 1], dp[i / 3]) + 1;
 				
 			} else {
-				arr[i] = arr[i - 1] + 1;
+				dp[i] = dp[i - 1] + 1;
 			}
 		}
 		
-		bw.write(Integer.toString(arr[n]));
-		bw.flush();
+		System.out.println(dp[N]);
 
 		br.close();
-		bw.close();
 	}
-	
 }
