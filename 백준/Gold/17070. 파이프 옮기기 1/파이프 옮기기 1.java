a6c0916dc9f0861dff29bf9c6407757e5bb35669
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-02-28 11:06:40 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -11,30 +11,28 @@ public class Main {
 		int arr[][][] = new int[N + 1][N + 1][3]; // 동, 남동, 남
 		boolean chk[][] = new boolean[N + 1][N + 1];
 		
-		for (int r = 0; r < N; r++) {
+		for (int r = 1; r <= N; r++) {
 			st = new StringTokenizer(br.readLine());
 			
-			for (int c = 0; c < N; c++) {
+			for (int c = 1; c <= N; c++) {
 				if (st.nextToken().charAt(0) == '1') chk[r][c] = true;
 			}
 		}
 		
-		arr[0][1][0] = 1;
+		arr[1][2][0] = 1;
 		
-		for (int r = 0; r < N; r++) {
-			for (int c = 1; c < N; c++) {
-				// 동
-				if (!chk[r][c + 1]) arr[r][c + 1][0] = arr[r][c][0] + arr[r][c][1]; 
+		for (int r = 1; r <= N; r++) {
+			for (int c = 3; c <= N; c++) {
+				if (chk[r][c]) continue;
 				
-				// 남동
-				if (!chk[r][c + 1] && !chk[r + 1][c + 1] && !chk[r + 1][c]) arr[r + 1][c + 1][1] = arr[r][c][0] + arr[r][c][1] + arr[r][c][2]; 
+				arr[r][c][0] = arr[r][c - 1][0] + arr[r][c - 1][1]; // 가로로 들어오는 것
+				arr[r][c][2] = arr[r - 1][c][1] + arr[r - 1][c][2]; // 세로로 들어오는 것
 				
-				// 남
-				if (!chk[r + 1][c]) arr[r + 1][c][2] = arr[r][c][1] + arr[r][c][2];
+				if (!chk[r - 1][c] && !chk[r][c - 1]) arr[r][c][1] = arr[r - 1][c - 1][0] + arr[r - 1][c - 1][1] + arr[r - 1][c - 1][2]; 
 			}
 		}
 		
-		System.out.println(arr[N - 1][N - 1][0] + arr[N - 1][N - 1][1] + arr[N - 1][N - 1][2]);
+		System.out.println(arr[N][N][0] + arr[N][N][1] + arr[N][N][2]);
 		
 		br.close();
 	}
