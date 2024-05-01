273add9933c20ce8612ce2615971dade4a4ac323
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-05-01 17:08:39 +0900

    [Gold III] Title: 파일 합치기, Time: 500 ms, Memory: 31572 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int K, dp[][], sum[], end;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -10,10 +11,10 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         for (int run = 0; run < T; run++) {
-            int K = Integer.parseInt(br.readLine());
+            K = Integer.parseInt(br.readLine());
 
-            int dp[][] = new int[K + 1][K + 1];
-            int sum[] = new int[K + 1];
+            dp = new int[K + 1][K + 1];
+            sum = new int[K + 1];
 
             st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= K; i++) {
@@ -25,12 +26,14 @@ public class Main {
 
             for (int s = 2; s < K; s++) {
                 for (int r = 1; r <= K - s; r++) {
-                    int end = r + s;
-                    dp[r][end] = sum[end] - sum[r - 1] + Math.min(dp[r + 1][end], dp[r][end - 1]);
+                    end = r + s;
+                    dp[r][end] = Math.min(dp[r + 1][end], dp[r][end - 1]);
 
                     for (int c = r + 1; c < end - 1; c++) {
-                        dp[r][end] = Math.min(dp[r][end], sum[end] - sum[r - 1] + dp[r][c] + dp[c + 1][end]);
+                        dp[r][end] = Math.min(dp[r][end], dp[r][c] + dp[c + 1][end]);
                     }
+
+                    dp[r][end] += sum[end] - sum[r - 1];
                 }
             }
 
