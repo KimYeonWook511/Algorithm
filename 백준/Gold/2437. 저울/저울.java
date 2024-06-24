f45db24475dc1c8068866a44582305dede705d63
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-06-24 23:00:28 +0900

    [Gold II] Title: 저울, Time: 136 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > sum + 1) break;
+
+            sum += arr[i];
+        }
+
+        System.out.println(sum + 1);
+
+        br.close();
+    }
+}
