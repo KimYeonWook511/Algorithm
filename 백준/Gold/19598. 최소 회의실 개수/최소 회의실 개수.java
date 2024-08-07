cadc8038195edf4f5813868555e248362232b670
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-08-07 22:45:13 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 656 ms, Memory: 50320 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int arr[][] = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
+
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o1, o2);
+            }
+        });
+        pq.offer(arr[0][1]);
+
+        for (int i = 1; i < N; i++) {
+            if (pq.peek() <= arr[i][0]) pq.poll();
+
+            pq.offer(arr[i][1]);
+        }
+
+        System.out.println(pq.size());
+
+        br.close();
+    }
+}
