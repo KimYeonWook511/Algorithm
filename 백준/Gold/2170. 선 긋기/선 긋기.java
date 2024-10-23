85d3f6ecdd58fda8e1857dca12b8c187f5aced93
KimYeonWook511 <102712607+KimYeonWook511@users.noreply.github.com>
2024-10-23 23:16:28 +0900

    [Gold V] Title: 선 긋기, Time: 1568 ms, Memory: 275780 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -7,15 +7,7 @@ public class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int arr[][] = new int[N][2];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
-        
-        Arrays.sort(arr, new Comparator<int[]>() {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 if (o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
@@ -24,17 +16,25 @@ public class Main {
             }
         });
 
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            pq.offer(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
+        
         int sum = 0;
-        int pre[] = arr[0];
+        int pre[] = pq.poll();
+        int cur[];
         for (int i = 1; i < N; i++) {
-            if (pre[0] <= arr[i][0] && pre[1] >= arr[i][1]) continue;
+            cur = pq.poll();
 
-            if (pre[1] < arr[i][0]) {
+            if (pre[0] <= cur[0] && pre[1] >= cur[1]) continue;
+
+            if (pre[1] < cur[0]) {
                 sum += pre[1] - pre[0];
-                pre[0] = arr[i][0];
+                pre[0] = cur[0];
             } 
 
-            pre[1] = arr[i][1];
+            pre[1] = cur[1];
         }
 
         System.out.println(sum + pre[1] - pre[0]);
