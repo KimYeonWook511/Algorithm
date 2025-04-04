1e4252334a7d3c62b18e3ba67f32bdafffbe8e53
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-04-04 16:09:04 +0900

    [Gold II] Title: 나누기, Time: 264 ms, Memory: 29440 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.java
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -2,43 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, sum[], pieceVal;
-    static long dp[][];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        sum = new int[N + 1];
-        dp = new long[3][N + 1];
+        int N = Integer.parseInt(br.readLine());
+        int sum[] = new int[N + 1];
+        long dp[][] = new long[N][4];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+        }
 
-            for (int r = 0; r < 3; r++) {
-                dp[r][i] = -1;
+        if (sum[N] % 4 == 0) { // 4의 배수 체크해야 함 -> 안 하면 pieceVal이 이상해질 수 있음
+            int pieceVal = sum[N] / 4;
+    
+            // 3조각만 구하면 나머지는 pieceVal이 되기 때문에 N까지 돌지 않음
+            for (int i = 1; i < N; i++) {
+                dp[i][1] = dp[i - 1][1] + (sum[i] == pieceVal ? 1 : 0);
+    
+                for (int pieceCnt = 2; pieceCnt <= 3; pieceCnt++) {
+                    dp[i][pieceCnt] = dp[i - 1][pieceCnt];
+    
+                    if (sum[i] == pieceCnt * pieceVal) dp[i][pieceCnt] += dp[i - 1][pieceCnt - 1];
+                }
             }
         }
 
-        pieceVal = sum[N] / 4;
-
-        System.out.println(func(0, 1));
+        System.out.println(dp[N - 1][3]);
 
         br.close();
-    }
-
-    static long func(int pieceCnt, int startIdx) {
-        if (pieceCnt == 3) return pieceVal == sum[N] - sum[startIdx - 1] ? 1 : 0;
-        if (dp[pieceCnt][startIdx] != -1) return dp[pieceCnt][startIdx];
-
-        dp[pieceCnt][startIdx] = 0;
-
-        for (int endIdx = startIdx; endIdx <= N - 3 + pieceCnt; endIdx++) {
-            if (pieceVal != sum[endIdx] - sum[startIdx - 1]) continue;
-
-            dp[pieceCnt][startIdx] += func(pieceCnt + 1, endIdx + 1);
-        }
-
-        return dp[pieceCnt][startIdx];
     }
 }
