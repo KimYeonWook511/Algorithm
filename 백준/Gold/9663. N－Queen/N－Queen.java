959b5e335964d0220fe1850017ad0f776e173ec1
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-04-15 13:19:03 +0900

    [Gold IV] Title: N-Queen, Time: 2604 ms, Memory: 15092 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,48 +1,54 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static int n;
-	static int colArr[];
-	static int result;
-	
-	public static void main(String[] args) throws IOException {
-		
-		n = Integer.parseInt(br.readLine());
-		colArr = new int[n];
-		
-		for (int col = 0; col < n; col++) {
-			cal(0, col); // 첫 번째 퀸 말
-		}
-		
-		bw.write(Integer.toString(result));
-		bw.flush();
 
-		br.close();
-		bw.close();
-	}
+	static int T, N, arr[], cnt;
+	static boolean colChk[], crossChk[][];
 	
-	static void cal(int row, int col) {
-		for (int i = 0; i < row; i++) {
-			if (col == colArr[i]) return; // 같은 열에 위치
-			if (Math.abs(row - i) == Math.abs(col - colArr[i])) return; // 대각선에 위치
-		}
-		
-		if (row == n - 1) {
-			// 마지막 행
-			result++;
+	public static void main(String[] args) throws Exception {
+    	N = Integer.parseInt(br.readLine());
+    	
+    	arr = new int[N]; // 행은 배제(놓는 순서대로 행 순서라는 가정)
+    	colChk = new boolean[N];
+    	crossChk = new boolean[2][N << 1]; // 좌상, 우상  | 우하, 좌하 (시계방향) -> 아래는 고려할 필요 없음
+    	
+    	for (int c = 0; c < N; c++) {
+    		colChk[c] = true;
+    		crossChk[0][N - (c + 1)] = true; // 좌상
+    		crossChk[1][N - (N - c)] = true; // 우상
+    		arr[0] = c;
+    		func(1);
+    		colChk[c] = false;
+    		crossChk[0][N - (c + 1)] = false;
+    		crossChk[1][N - (N - c)] = false;
+    	}
+        	
+        System.out.println(cnt);
+
+        br.close();
+    }
+	
+	static void func(int r) {
+		if (r == N) {
+			cnt++;
+			
 			return;
 		}
 		
-		colArr[row] = col; // 퀸이 놓인 열 위치
-		
-		for (int i = 0; i < n; i++) {
-			cal(row + 1, i);
+		for (int c = 0; c < N; c++) {
+			if (colChk[c]) continue; // 열 체크
+			if (crossChk[0][(N + r) - (c + 1)] || crossChk[1][(N + r) - (N - c)]) continue; // 대각선 체크
+
+			colChk[c] = true;
+			crossChk[0][(N + r) - (c + 1)] = true; // 좌상
+			crossChk[1][(N + r) - (N - c)] = true; // 우상
+			arr[r] = c;
+			func(r + 1);
+			colChk[c] = false;
+			crossChk[0][(N + r) - (c + 1)] = false;
+			crossChk[1][(N + r) - (N - c)] = false;
 		}
 	}
 }
