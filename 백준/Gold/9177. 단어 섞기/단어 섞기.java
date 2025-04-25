79902929070443cbc926e40fb2adc533637af381
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-04-25 16:50:13 +0900

    [Gold IV] Title: 단어 섞기, Time: 604 ms, Memory: 286288 KB -BaekjoonHub

--- 백준/Gold/9177. 단어 섞기/단어 섞기.java
+++ 백준/Gold/9177. 단어 섞기/단어 섞기.java
@@ -10,29 +10,29 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            String str1 = st.nextToken();
-            String str2 = st.nextToken();
-            String target = st.nextToken();
+            char str1[] = st.nextToken().toCharArray();
+            char str2[] = st.nextToken().toCharArray();
+            char target[] = st.nextToken().toCharArray();
 
             Deque<int[]> deque = new ArrayDeque<>();
-            boolean chk[][] = new boolean[str1.length() + 1][str2.length() + 1];
+            boolean chk[][] = new boolean[str1.length + 1][str2.length + 1];
             deque.offer(new int[]{0, 0, 0});
 
             boolean flag = false;
             while (!deque.isEmpty()) {
                 int cur[] = deque.poll();
                 
-                if (cur[2] == target.length()) {
+                if (cur[2] == target.length) {
                     flag = true;
                     break;
                 }
 
-                if (cur[0] < str1.length() && !chk[cur[0] + 1][cur[1]] && str1.charAt(cur[0]) == target.charAt(cur[2])) {
+                if (cur[0] < str1.length && !chk[cur[0] + 1][cur[1]] && str1[cur[0]] == target[cur[2]]) {
                     deque.offer(new int[]{cur[0] + 1, cur[1], cur[2] + 1});
                     chk[cur[0] + 1][cur[1]] = true;
                 }
 
-                if (cur[1] < str2.length() && !chk[cur[0]][cur[1] + 1] && str2.charAt(cur[1]) == target.charAt(cur[2])) {
+                if (cur[1] < str2.length && !chk[cur[0]][cur[1] + 1] && str2[cur[1]] == target[cur[2]]) {
                     deque.offer(new int[]{cur[0], cur[1] + 1, cur[2] + 1});
                     chk[cur[0]][cur[1]] = true;
                 }
