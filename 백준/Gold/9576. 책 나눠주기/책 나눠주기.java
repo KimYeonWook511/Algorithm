15d2ced277f18f325b04ef504edc1bb0785effc4
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-05-07 16:33:15 +0900

    [Gold II] Title: 책 나눠주기, Time: 176 ms, Memory: 18728 KB -BaekjoonHub

--- 백준/Gold/9576. 책 나눠주기/책 나눠주기.java
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int arr[][], chkIdx[];
-    static boolean chk[];
+    static int parent[];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,20 +15,41 @@ public class Main {
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            arr = new int[M + 1][2];
-            chkIdx = new int[N + 1];
-            int cnt = 0;
+            int arr[][] = new int[M][2];
 
-            for (int i = 1; i <= M; i++) {
+            for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
 
                 arr[i][0] = a;
                 arr[i][1] = b;
+            }
 
-                chk = new boolean[N + 1];
-                if (dfs(i)) cnt++;
+            Arrays.sort(arr, new Comparator<int[]>() {
+                @Override
+                public int compare(int o1[], int o2[]) {
+                    return Integer.compare(o1[1], o2[1]);
+                }
+            });
+
+            parent = new int[N + 2];
+            for (int i = 1; i <= N; i++) {
+                parent[i] = i;
+            }
+            parent[N + 1] = N + 1; // 범위 초과 표시
+
+            int cnt = 0;
+            for (int i = 0; i < M; i++) {
+                int a = arr[i][0];
+                int b = arr[i][1];
+
+                int pointer = find(a);
+                if (pointer <= b) {
+                    // union
+                    parent[pointer] = find(pointer + 1);
+                    cnt++;
+                }
             }
 
             sb.append(cnt).append("\n");
@@ -40,20 +60,9 @@ public class Main {
         br.close();
     }
 
-    static boolean dfs(int cur) {
-        int s = arr[cur][0];
-        int e = arr[cur][1];
+    static int find(int x) {
+        if (x == parent[x]) return x;
 
-        for (; s <= e; s++) {
-            if (chk[s]) continue;
-
-            chk[s] = true;
-            if (chkIdx[s] == 0 || dfs(chkIdx[s])) {
-                chkIdx[s] = cur;
-                return true;
-            }
-        }
-
-        return false;
+        return parent[x] = find(parent[x]);
     }
 }
