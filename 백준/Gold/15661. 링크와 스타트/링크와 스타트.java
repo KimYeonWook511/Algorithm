1ede5dfe032bcd092a1ae7d459632181f8839678
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-07-05 13:48:32 +0900

    [Gold V] Title: 링크와 스타트, Time: 188 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     static int N, arr[][];
     static int result = Integer.MAX_VALUE;
-    static int fullSelectBit;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,41 +20,30 @@ public class Main {
             }
         }
 
-        fullSelectBit = (1 << N) - 1; // 한 쪽 팀으로 되는 경우 빼기
-        func(1, 1); // 0번은 스타트팀 고정 -> 백트래킹
+        func(1, 1, 0, 0); // 0번은 스타트팀 고정 -> 백트래킹
 
         System.out.println(result);
 
         br.close();
     }
 
-    static void func(int cur, int selectBit) {
+    static void func(int cur, int selectBit, int startTeam, int linkTeam) {
         if (cur == N) {
-            if (selectBit != fullSelectBit) cal(selectBit);
-
+            result = Math.min(result, Math.abs(startTeam - linkTeam));
             return;
         }
 
-        // 선택 -> 스타트팀
-        func(cur + 1, selectBit | (1 << cur));
-
-        // 미선택 -> 링크팀
-        func(cur + 1, selectBit);
-    }
-
-    static void cal(int startTeamBit) {
-        int startTeam = 0;
-        int linkTeam = 0;
-
-        for (int i = 0; i < N; i++) {
-            boolean isStartTeam = (startTeamBit & (1 << i)) > 0;
-
-            for (int k = i + 1; k < N; k++) {
-                if (isStartTeam && (startTeamBit & (1 << k)) > 0) startTeam += arr[i][k] + arr[k][i];
-                else if (!isStartTeam && (startTeamBit & (1 << k)) == 0) linkTeam += arr[i][k] + arr[k][i];
-            }
+        int startTemp = 0;
+        int linkTemp = 0;
+        for (int i = 0; i < cur; i++) {
+            if ((selectBit & (1 << i)) > 0) startTemp += arr[cur][i] + arr[i][cur];
+            else linkTemp += arr[cur][i] + arr[i][cur];
         }
 
-        result = Math.min(result, Math.abs(startTeam - linkTeam));
+        // 선택 -> 스타트팀
+        func(cur + 1, selectBit | (1 << cur), startTeam + startTemp, linkTeam);
+
+        // 미선택 -> 링크팀
+        func(cur + 1, selectBit, startTeam, linkTeam + linkTemp);
     }
 }
