1c73b25bbfe69d1ba7a8025e85beec4b14903ab0
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-08-08 16:42:21 +0900

    [Gold IV] Title: 거짓말, Time: 584 ms, Memory: 67812 KB -BaekjoonHub

--- 백준/Gold/31091. 거짓말/거짓말.java
+++ 백준/Gold/31091. 거짓말/거짓말.java
@@ -8,31 +8,28 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine());
 
-		int left[] = new int[N + 1]; // 진실에서 거짓으로
-		int right[] = new int[N + 1]; // 거짓에서 진실로
-		int cnt = 0; // 거짓말을 하고 있는 사람 수
+		int cnt[] = new int[N + 2];
+		int liarCnt = 0; // 거짓말을 하고 있는 사람 수
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			int k = Integer.parseInt(st.nextToken());
 
-			if (k <= 0) left[-k]++;
+			if (k <= 0) cnt[-k + 1]++;
 			else {
-				right[k]++;
-				cnt++;
+				cnt[k]--;
+				liarCnt++;
 			}
 		}
 
 		int result = 0;
 		for (int i = 0; i <= N; i++) {
-			cnt -= right[i]; // 거짓이던 사람이 진실이 됨
+			liarCnt += cnt[i];
 
-			if (cnt == i) {
+			if (liarCnt == i) {
 				result++;
 				sb.append(i).append(" ");
 			}
-
-			cnt += left[i]; // 진실이던 사람이 거짓이 됨
 		}
 
 		System.out.println(result);
