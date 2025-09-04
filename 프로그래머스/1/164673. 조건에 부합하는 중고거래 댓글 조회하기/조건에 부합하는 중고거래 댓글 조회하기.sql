62e56b68c2eb63d6f26d721ad2383b7b1538c798
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-09-04 18:40:46 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -3,8 +3,16 @@ SELECT ugb.title, ugb.board_id, ugr.reply_id, ugr.writer_id, ugr.contents, date_
     from used_goods_board ugb
     join used_goods_reply ugr
       on ugb.board_id = ugr.board_id
-      # and ugr.created_date >= "2022-10-01"
-      # and ugr.created_date < "2022-11-01"
     where ugb.created_date >= "2022-10-01"
       and ugb.created_date < "2022-11-01"
     order by ugr.created_date, ugb.title;
+    
+/*
+SELECT ugb.title, ugb.board_id, ugr.reply_id, ugr.writer_id, ugr.contents, date_format(ugr.created_date, "%Y-%m-%d")
+    from used_goods_board ugb
+    join used_goods_reply ugr
+      on ugb.board_id = ugr.board_id
+      and ugb.created_date >= "2022-10-01"
+      and ugb.created_date < "2022-11-01"
+    order by ugr.created_date, ugb.title;
+*/
