4e77eb5c176f71afeef1590186bac2c357e85e4b
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-09-12 18:39:04 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,10 @@
+select id, 
+        case ntile(4) over (order by size_of_colony desc)
+            when 1 then "CRITICAL"
+            when 2 then "HIGH"
+            when 3 then "MEDIUM"
+            when 4 then "LOW"
+        end as colony_name
+  from ecoli_data
+  order by id;
+        
