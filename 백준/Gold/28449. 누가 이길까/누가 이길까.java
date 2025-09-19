1d138ddc8a79d4019bef061ee42476243ecf5a46
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-09-19 15:34:30 +0900

    [Gold V] Title: 누가 이길까, Time: 636 ms, Memory: 42864 KB -BaekjoonHub

---
+++ 백준/Gold/28449. 누가 이길까/누가 이길까.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int HI[] = new int[N];
+        int cntHI[] = new int[100_001];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            HI[i] = Integer.parseInt(st.nextToken());
+            cntHI[HI[i]]++;
+        }
+
+        int ARC[] = new int[M];
+        int cntARC[] = new int[100_001];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            ARC[i] = Integer.parseInt(st.nextToken());
+            cntARC[ARC[i]]++;
+        }
+
+        Arrays.sort(HI);
+        Arrays.sort(ARC);
+
+        long result[] = new long[3];
+        int left = 0;
+        for (int i = 0; i < N; i++) {
+            int right = M - 1;
+            while (left <= right) {
+                int mid = (left + right) >> 1;
+
+                if (HI[i] >= ARC[mid]) left = mid + 1;
+                else right = mid - 1;
+            }
+
+            result[0] += (right + 1) - cntARC[HI[i]];
+            result[1] += M - (right + 1);
+            result[2] += cntARC[HI[i]];
+        }
+
+        System.out.println(result[0] + " " + result[1] + " " + result[2]);
+
+        br.close();
+    }
+}
