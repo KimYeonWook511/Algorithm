6db972ed91eed388ad874b3a183712c997f5ed4b
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-11-18 21:30:32 +0900

    [Gold IV] Title: Count Circle Groups, Time: 2508 ms, Memory: 123804 KB -BaekjoonHub

---
+++ 백준/Gold/10216. Count Circle Groups/Count Circle Groups.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Area {
+        int x, y, R;
+
+        Area (int x, int y, int R) {
+            this.x = x;
+            this.y = y;
+            this.R = R;
+        }
+    }
+    static int parent[];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            Area areas[] = new Area[N];
+            parent = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                int R = Integer.parseInt(st.nextToken());
+                areas[i] = new Area(x, y, R);
+                parent[i] = i;
+            }
+
+            int answer = N;
+            for (int i = 0; i < N; i++) {
+                for (int k = i + 1; k < N; k++) {
+                    int x = areas[i].x - areas[k].x;
+                    int y = areas[i].y - areas[k].y;
+                    int R = areas[i].R + areas[k].R;
+
+                    if (x * x + y * y > R * R) continue;
+
+                    if (union(i, k)) {
+                        answer--;
+                    }
+                }
+            }
+
+            sb.append(answer).append("\n");
+        }
+
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    static int find(int x) {
+        if (x == parent[x]) return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    static boolean union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y) return false;
+
+        if (x < y) {
+            parent[y] = x;
+        } else {
+            parent[x] = y;
+        }
+
+        return true;
+    }
+}
