9007ac5df8736b8e6a48381cdef0b819bff467e8
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2025-11-19 23:10:30 +0900

    [Silver I] Title: 본대 산책, Time: 120 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/12849. 본대 산책/본대 산책.java
+++ 백준/Silver/12849. 본대 산책/본대 산책.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     static final int MOD = 1_000_000_007;
@@ -11,24 +10,20 @@ public class Main {
 
         // 0: 정보과학관 / 1: 전산관 / 2: 미래관 / 3: 신양관
         // 4: 한경직기념관 / 5: 진리관 / 6: 학생회관 / 7: 형남공학관
-        List<Integer> graph[] = new List[8];
-        for (int i = 0; i < 8; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        initGraph(graph);
-
-        int dp[][] = new int[2][8];
+        long dp[][] = new long[2][8];
         dp[1][1] = 1;
         dp[1][2] = 1;
         int pre = 1;
         int cur = 0;
         for (int i = 2; i <= D; i++) {
-            for (int j = 0; j < 8; j++) {
-                dp[cur][j] = 0;
-                for (int preNode : graph[j]) {
-                    dp[cur][j] = (dp[cur][j] + dp[pre][preNode]) % MOD;
-                }
-            }
+            dp[cur][0] = (dp[pre][1] + dp[pre][2]) % MOD;
+            dp[cur][1] = (dp[pre][0] + dp[pre][2] + dp[pre][3]) % MOD;
+            dp[cur][2] = (dp[pre][0] + dp[pre][1] + dp[pre][3] + dp[pre][4]) % MOD;
+            dp[cur][3] = (dp[pre][1] + dp[pre][2] + dp[pre][4] + dp[pre][5]) % MOD;
+            dp[cur][4] = (dp[pre][2] + dp[pre][3] + dp[pre][5] + dp[pre][7]) % MOD;
+            dp[cur][5] = (dp[pre][3] + dp[pre][4] + dp[pre][6]) % MOD;
+            dp[cur][6] = (dp[pre][5] + dp[pre][7]) % MOD;
+            dp[cur][7] = (dp[pre][4] + dp[pre][6]) % MOD;
             pre = cur;
             cur = 1 - cur;
         }
@@ -36,47 +31,5 @@ public class Main {
         System.out.println(dp[pre][0]);
 
         br.close();
-    }
-
-    static void initGraph(List<Integer> graph[]) {
-        // 정보과학관
-        graph[0].add(1);
-        graph[0].add(2);
-
-        // 전산관
-        graph[1].add(0);
-        graph[1].add(2);
-        graph[1].add(3);
-
-        // 미래관
-        graph[2].add(0);
-        graph[2].add(1);
-        graph[2].add(3);
-        graph[2].add(4);
-
-        // 신양관
-        graph[3].add(1);
-        graph[3].add(2);
-        graph[3].add(4);
-        graph[3].add(5);
-
-        // 한경직기념관
-        graph[4].add(2);
-        graph[4].add(3);
-        graph[4].add(5);
-        graph[4].add(7);
-
-        // 진리관
-        graph[5].add(3);
-        graph[5].add(4);
-        graph[5].add(6);
-
-        // 학생회관
-        graph[6].add(5);
-        graph[6].add(7);
-
-        // 형남공학관
-        graph[7].add(4);
-        graph[7].add(6);
     }
 }
