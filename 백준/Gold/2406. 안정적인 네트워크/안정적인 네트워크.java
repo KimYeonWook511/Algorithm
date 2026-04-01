6fc5dba54abb8e439612a95150132d5bf2f4dfdc
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2026-04-01 15:46:15 +0900

    [Gold III] Title: 안정적인 네트워크, Time: 1608 ms, Memory: 127676 KB -BaekjoonHub

--- 백준/Gold/2406. 안정적인 네트워크/안정적인 네트워크.java
+++ 백준/Gold/2406. 안정적인 네트워크/안정적인 네트워크.java
@@ -2,21 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Node {
+    static class Edge {
         int from, to, w;
 
-        Node(int from, int to, int w) {
+        Edge(int from, int to, int w) {
             this.from = from;
             this.to = to;
             this.w = w;
         }
     }
+    static int parents[];
+    static int ranks[];
 
     public static void main(String[] args) throws Exception {
         // 1을 제외하고 연결되어야 함
         // 프림, 크루스칼 다 가능해 보임
-        solvePrim();
-        // solveKruskal();
+        // solvePrim();
+        solveKruskal();
     }
 
     static void solvePrim() throws Exception {
@@ -26,7 +28,7 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        List<Node> graph[] = new List[n + 1];
+        List<Edge> graph[] = new List[n + 1];
         int dist[] = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
@@ -37,8 +39,8 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            graph[x].add(new Node(x, y, 0));
-            graph[y].add(new Node(y, x, 0));
+            graph[x].add(new Edge(x, y, 0));
+            graph[y].add(new Edge(y, x, 0));
         }
 
         for (int x = 1; x <= n; x++) {
@@ -51,19 +53,19 @@ public class Main {
                 if (x == y) continue;
                 if (y == 1) continue;
 
-                graph[x].add(new Node(x, y, w));
-                graph[y].add(new Node(y, x, w));
+                graph[x].add(new Edge(x, y, w));
+                graph[y].add(new Edge(y, x, w));
             }
         }
 
-        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
+        PriorityQueue<Edge> pq = new PriorityQueue<>(new Comparator<Edge>() {
             @Override
-            public int compare(Node o1, Node o2) {
+            public int compare(Edge o1, Edge o2) {
                 return Integer.compare(o1.w, o2.w);
             }
         });
         boolean visited[] = new boolean[n + 1];
-        pq.offer(new Node(0, 2, 0));
+        pq.offer(new Edge(0, 2, 0));
         dist[2] = 0;
 
         int X = 0;
@@ -72,7 +74,7 @@ public class Main {
 
         int cnt = n - 1;
         while (!pq.isEmpty()) {
-            Node cur = pq.poll();
+            Edge cur = pq.poll();
 
             if (visited[cur.to]) continue;
             visited[cur.to] = true;
@@ -85,7 +87,7 @@ public class Main {
 
             if (--cnt == 0) break;
 
-            for (Node next : graph[cur.to]) {
+            for (Edge next : graph[cur.to]) {
                 if (visited[next.to]) continue;
                 if (dist[next.to] <= next.w) continue;
 
@@ -100,7 +102,95 @@ public class Main {
         br.close();
     }
 
-    static void solveKruskal() {
+    static void solveKruskal() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parents = new int[n + 1];
+        ranks = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parents[i] = i;
+        }
+
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(x, y, 0));
+        }
+
+        for (int x = 1; x <= n; x++) {
+            st = new StringTokenizer(br.readLine());
+            if (x == 1) continue;
+
+            for (int y = 1; y <= n; y++) {
+                int w = Integer.parseInt(st.nextToken());
+
+                if (x == y) continue;
+                if (y == 1) continue;
+
+                edges.add(new Edge(x, y, w));
+            }
+        }
+
+        Collections.sort(edges, new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return Integer.compare(o1.w, o2.w);
+            }
+        });
+
+        int X = 0;
+        int K = 0;
+        StringBuilder sb = new StringBuilder();
+
+        int cnt = n - 1;
+        for (Edge edge : edges) {
+            int from = edge.from;
+            int to = edge.to;
+            int w = edge.w;
+
+            if (union(from, to)) {
+                if (w != 0) {
+                    X += w;
+                    K++;
+                    sb.append(from).append(" ").append(to).append("\n");
+                }
+
+                if (--cnt == 0) break;
+            }
+        }
+
+        System.out.println(X + " " + K);
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    static int find(int x) {
+        if (parents[x] == x) return x;
+
+        return parents[x] = find(parents[x]);
+    }
+
+    static boolean union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y) return false;
+
+        if (ranks[x] < ranks[y]) {
+            parents[x] = y;
+        } else if (ranks[x] > ranks[y]) {
+            parents[y] = x;
+        } else {
+            parents[x] = y;
+            ranks[y]++;
+        }
+        return true;
     }
 }
