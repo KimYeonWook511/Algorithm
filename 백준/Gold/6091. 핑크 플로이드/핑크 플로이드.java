b955ca2c5a72bd7de365a1b7bf646feb03e39be1
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2026-04-24 15:49:25 +0900

    [Gold I] Title: 핑크 플로이드, Time: 1580 ms, Memory: 73380 KB -BaekjoonHub

--- 백준/Gold/6091. 핑크 플로이드/핑크 플로이드.java
+++ 백준/Gold/6091. 핑크 플로이드/핑크 플로이드.java
@@ -10,25 +10,46 @@ public class Main {
             this.w = w;
         }
     }
+    static class Edge {
+        int u, v;
+        int w;
+
+        Edge (int u, int v, int w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        int arr[][] = new int[N + 1][N + 1];
+        // 프림 전용 input 처리
+        // int arr[][] = new int[N + 1][N + 1];
+        // for (int r = 1; r < N; r++) {
+        //     StringTokenizer st = new StringTokenizer(br.readLine());
+        //     for (int c = r + 1; c <= N; c++) {
+        //         arr[r][c] = Integer.parseInt(st.nextToken());
+        //     }
+        //     arr[r][r] = Integer.MAX_VALUE;
+        // }
+
+        // 크루스칼 전용 input 처리
+        Edge edges[] = new Edge[N * (N - 1) / 2];
+        int index = 0;
         for (int r = 1; r < N; r++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int c = r + 1; c <= N; c++) {
-                arr[r][c] = Integer.parseInt(st.nextToken());
+                edges[index++] = new Edge(r, c, Integer.parseInt(st.nextToken()));
             }
-            arr[r][r] = Integer.MAX_VALUE;
         }
 
         // 크루스칼
-        // List<Integer> anwer[] = kruskal(N, arr);
+        List<Integer> answer[] = kruskal(N, edges);
         // 프림
-        List<Integer> answer[] = prim(N, arr);
+        // List<Integer> answer[] = prim(N, arr);
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
@@ -45,6 +66,60 @@ public class Main {
         System.out.println(sb);
 
         br.close();
+    }
+
+    static List[] kruskal(int N, Edge edges[]) {
+        List<Integer> graph[] = new List[N + 1];
+        int parents[] = new int[N + 1];
+        int ranks[] = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+            parents[i] = i;
+        }
+
+        Arrays.sort(edges, new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return Integer.compare(o1.w, o2.w);
+            }
+        });
+
+        int cnt = N - 1;
+        for (Edge edge : edges) {
+            if (union(edge.u, edge.v, parents, ranks)) {
+                graph[edge.u].add(edge.v);
+                graph[edge.v].add(edge.u);
+
+                if (--cnt == 0) {
+                    break;
+                }
+            }
+        }
+
+        return graph;
+    }
+
+    static int find(int x, int parents[]) {
+        if (parents[x] == x) return x;
+
+        return parents[x] = find(parents[x], parents);
+    }
+
+    static boolean union(int x, int y, int parents[], int ranks[]) {
+        x = find(x, parents);
+        y = find(y, parents);
+
+        if (x == y) return false;
+
+        if (ranks[x] < ranks[y]) {
+            parents[x] = y;
+        } else if (ranks[x] > ranks[y]) {
+            parents[y] = x;
+        } else {
+            parents[y] = x;
+            ranks[x]++;
+        }
+        return true;
     }
 
     static List[] prim(int N, int arr[][]) {
