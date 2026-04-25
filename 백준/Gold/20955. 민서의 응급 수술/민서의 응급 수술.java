d142355da21a23fa4738cb73ba4300d351913ba9
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2026-04-25 14:56:44 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 316 ms, Memory: 40680 KB -BaekjoonHub

---
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int parent[];
+    static int rank[];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        rank = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        int count = N - 1;
+        int cycle = 0;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            if (find(u) == find(v)) {
+                cycle++;
+            } else {
+                union(u, v);
+                count--;
+            }
+        }
+
+        int answer = cycle + count;
+        System.out.println(answer);
+
+        br.close();
+    }
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a == b) return;
+
+        if (rank[a] < rank[b]) {
+            parent[a] = b;
+        } else if (rank[a] > rank[b]) {
+            parent[b] = a;
+        } else {
+            parent[b] = a;
+            rank[a]++;
+        }
+    }
+}
